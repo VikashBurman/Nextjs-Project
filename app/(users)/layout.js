035d865b0f0d41d822871ebf4714f6e24da0626b
Vikash Burman <vikasburman37@gmail.com>
2025-07-24 17:12:44 +0530

    Added route group for user and admin

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css"
+import "../globals.css"
 import Navbar from "./componets/navbar";
 
 export default function RootLayout({ children }) {
---
+++ app/admin/about/page.js
@@ -0,0 +1,9 @@
+const About = () =>{
+  return(
+    <>
+      <h1>About Admin page</h1>
+    </>
+  )
+}
+
+export default About;
